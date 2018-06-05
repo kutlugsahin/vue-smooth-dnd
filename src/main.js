6b689a3a3e8f83e30a6d7178d613f643bd424271
Kutlu Sahin <ksahin@sdl.com>
2018-06-05 16:53:24 +0200

    demo polyfills

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Demo from './demo';
+import './polyfills';
 
 Vue.config.productionTip = false
 
