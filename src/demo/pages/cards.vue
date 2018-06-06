75c4f646f28b4455adeb4c894d4f37119d95d0d9
Kutlu Sahin <ksahin@sdl.com>
2018-06-06 16:57:16 +0200

    removed tag props from cards

--- src/demo/pages/cards.vue
+++ src/demo/pages/cards.vue
@@ -20,9 +20,8 @@
 							:get-child-payload="getCardPayload(column.id)"
 							drag-class="card-ghost"
 							drop-class="card-ghost-drop"
-              :tag="{value: 'table', props: {class: 'sdfsdf'}}"
 						>
-							<Draggable v-for="card in column.children" :key="card.id" tag="tr">
+							<Draggable v-for="card in column.children" :key="card.id">
 								<div :class="card.props.className" :style="card.props.style">
 									<p>
 										{{card.data}}
