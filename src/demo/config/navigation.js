49ad52e74ed5fd16ff84cd0acd3b2be003a8f48a
Dave Stewart <dave@davestewart.co.uk>
2018-09-18 20:08:13 +0100

    Simplified component import

--- src/demo/config/navigation.js
+++ src/demo/config/navigation.js
@@ -1,55 +1,35 @@
-import Cards from '../pages/cards';
-import Form from "../pages/form";
-
-import Simple from '../pages/simple';
-import SimpleScroller from '../pages/simple-scroller';
-import SimpleHorizontal from '../pages/simple-horizontal';
-
-import Groups from '../pages/groups';
-import Copy from '../pages/copy';
-import Nested from '../pages/nested';
-
-import LockAxis from '../pages/lock-axis';
-import DragHandle from '../pages/drag-handle';
-import DragClass from '../pages/drag-class';
-import DragDelay from '../pages/drag-delay';
-import TransitionDuration from '../pages/transition-duration';
-import Table from '../pages/table';
-
-import Events from '../pages/events';
-
 function section (title, pages) {
   return { title, pages }
 }
 
-function page (name, title, component) {
-  return { name, title, component }
+function page (name, title) {
+  return { name, title, component: require(`../pages/${name}.vue`).default }
 }
 
 export default [
   section('Showcase', [
-    page('cards', 'Card board', Cards),
-    page('form', 'Form elements', Form)
+    page('cards', 'Card board'),
+    page('form', 'Form elements')
   ]),
   section('Sortables', [
-    page('simple', 'Sortable with default options', Simple),
-    page('simple-scroller', 'Sortable inside scroller', SimpleScroller),
-    page('simple-horizontal', 'Horizontal sortable', SimpleHorizontal)
+    page('simple', 'Sortable with default options'),
+    page('simple-scroller', 'Sortable inside scroller'),
+    page('simple-horizontal', 'Horizontal sortable')
   ]),
   section('Groups', [
-    page('groups', 'DnD between groups', Groups),
-    page('copy', 'Copy draggable', Copy),
-    page('nested', 'Nested vertical sortable', Nested)
+    page('groups', 'DnD between groups'),
+    page('copy', 'Copy draggable'),
+    page('nested', 'Nested vertical sortable')
   ]),
   section('Properties', [
-    page('lock-axis', 'Lock axis', LockAxis),
-    page('drag-handle', 'Drag handle', DragHandle),
-    page('drag-class', 'Drag and Drop classses', DragClass),
-    page('drag-delay', 'Drag begin delay of 500ms', DragDelay),
-    page('transition-duration', 'Animation duration 500ms', TransitionDuration),
-    page('table', 'Custom Tags / Table', Table),
+    page('lock-axis', 'Lock axis'),
+    page('drag-handle', 'Drag handle'),
+    page('drag-class', 'Drag and Drop classes'),
+    page('drag-delay', 'Drag begin delay of 500ms'),
+    page('transition-duration', 'Animation duration 1000ms'),
+    page('table', 'Custom tag (table)')
   ]),
   section('Interaction', [
-    page('events', 'Callbacks and Events', Events),
+    page('events', 'Callbacks and Events')
   ])
 ]
